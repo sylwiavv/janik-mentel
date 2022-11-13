01a2017618e35a57d2abaafdfd83f58e520487f3
sylwia <sylwiavv@gmail.com>
2022-11-13 17:22:49 +0100

    WIP: add carousel for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,44 @@
 import * as React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { ImageContainer } from './Header.styles';
 import Navigation from '../Navigation/Navigation';
 
-export const Header = () => (
-  <ImageContainer>
-    {/* <img src="https://www.janik-mentel.pl/wp-content/uploads/2018/05/J-D-1006.jpg" alt="" /> */}
-    <Navigation />
-  </ImageContainer>
-);
+export const Header = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          src="https://www.janik-mentel.pl/wp-content/uploads/2018/05/A-Sz-0805.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          src="https://www.janik-mentel.pl/wp-content/uploads/2018/05/A-Sz-0883.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+  // <ImageContainer>
+  //   {/* <img src="https://www.janik-mentel.pl/wp-content/uploads/2018/05/J-D-1006.jpg" alt="" /> */}
+  //   <Navigation />
+  // </ImageContainer>
+};
