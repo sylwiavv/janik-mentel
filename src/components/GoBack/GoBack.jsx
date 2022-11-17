49f43c6700ea35c9703722515c4449e3866a56a3
sylwia <sylwiavv@gmail.com>
2022-11-17 23:04:45 +0100

    Add go back component

---
+++ src/components/GoBack/GoBack.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+import ArrowLeftIcon from '../icons/ArrowLeftIcon';
+import { Box } from '../Boxes/Boxes.styles';
+
+export default function GoBack() {
+  return (
+    <Box isButton="pointer" gap="16px" isCenter onClick={() => navigate(-1)}>
+      <ArrowLeftIcon height="24px" />
+      <span>Wróć</span>
+    </Box>
+  );
+}
