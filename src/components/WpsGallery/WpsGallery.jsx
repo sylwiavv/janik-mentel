cd55d06f1b6b393e9edca2625ae38998c9de1dcb
sylwia <sylwiavv@gmail.com>
2022-11-18 12:18:45 +0100

    Create Wsp gallery component
    - add key handling
    - create close icon component
    - add example gallery

---
+++ src/components/WpsGallery/WpsGallery.jsx
@@ -0,0 +1,100 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import './WspGallery.css';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
+import ArrowLeftIcon from '../icons/ArrowLeftIcon';
+import { StyledIconClose } from '../Navigation/Navigation.styles';
+import CloseIcon from '../icons/CloseIcon';
+
+const WSPGallery = ({ galleryImages }) => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+    toggleBodyScroll();
+  };
+
+  const handleKeyDown = useCallback((event) => {
+    if (event.key === 'Escape') { handleCloseModal(); }
+    if (event.key === 'ArrowRight') { nextSlide(); }
+    if (event.key === 'ArrowLeft') { prevSlide(); }
+  }, [slideNumber]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown, false);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown, false);
+    };
+  }, [slideNumber]);
+
+  // TODO make it more reusal
+  const toggleBodyScroll = () => {
+    setOpenModal(!openModal);
+    document.body.classList.remove('not-scroll');
+
+    if (!openModal) {
+      document.body.classList.add('not-scroll');
+    }
+  };
+
+  // Close Modal
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  // Previous Image
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImages.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+
+  // Next Image
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImages.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
+  return (
+    <div>
+
+      {openModal
+                && (
+                <div className="sliderWrap">
+                  <CloseIcon className="btnClose" onClick={handleCloseModal} />
+                  <ArrowLeftIcon height="40px" className="btnPrev" onClick={prevSlide} />
+                  <ArrowRightIcon height="40px" className="btnNext" onClick={nextSlide} />
+                  <div className="fullScreenImage">
+                    <img src={galleryImages[slideNumber].img} alt="" />
+                  </div>
+                </div>
+                )}
+
+      {/* <br />
+      Current slide number:  {slideNumber}
+      <br />
+      Total Slides: {galleryImages.length}
+      <br /><br /> */}
+
+      <div className="galleryWrap">
+        {
+                    galleryImages && galleryImages.map((slide, index) => (
+                      <div
+                        className="single"
+                        key={index}
+                        onClick={() => handleOpenModal(index)}
+                      >
+                        <img src={slide.img} alt="" />
+                      </div>
+                    ))
+                }
+      </div>
+
+    </div>
+  );
+};
+
+export default WSPGallery;
--- src/templates/GaleriaTemplate.jsx
+++ src/templates/GaleriaTemplate.jsx
@@ -1,24 +1,43 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { graphql } from 'gatsby';
-import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
+// import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
 import { Box } from '../components/Boxes/Boxes.styles';
 import GoBack from '../components/GoBack/GoBack';
+import WpsGallery from '../components/WpsGallery/WpsGallery';
+import WSPGallery from '../components/WpsGallery/WpsGallery';
 
+const galleryImages = [
+  {
+    img: 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    img: 'https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    img: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    img: 'https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    img: 'https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    img: 'https://images.pexels.com/photos/1712/sunglasses-apple-iphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
 const GaleriaTemplate = ({ data: { post: { galeriaTitle, galeriaAsets } } }) => (
   <div>
     <GoBack />
     <Box isGap>
-      <SimpleReactLightbox>
-        {galeriaAsets.map(({ url }, i) => (
-          <SRLWrapper>
-            <img key={i} src={url} alt="" />
-          </SRLWrapper>
-        ))}
-      </SimpleReactLightbox>
-
+      {galeriaAsets.map(({ url }, i) => (
+        <img key={i} src={url} alt="" />
+      ))}
     </Box>
-
+    <WSPGallery
+      galleryImages={galleryImages}
+    />
   </div>
 );
 
